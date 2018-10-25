cde3a5812ef2e5db7bd1c01df929822964a635c5
DanFreder <freder.daniel@gmail.com>
2018-10-25 11:44:03 -0400

    Ex5: Started Exercise 5

--- exercises/inClassFuckery/js/script.js
+++ exercises/inClassFuckery/js/script.js
@@ -1,10 +1,12 @@
 var ball1;
 var ball2;
+var ball3;
 
 function setup() {
   createCanvas(640, 480);
   ball1 = new Ball(10, 100, 5, -3, 10, 1);
   ball2 = new Ball(50, 200, -3, 5, 20, 1);
+  ball3 = new Ball(25,150,4,-4,15,1);
 }
 
 function draw() {
@@ -13,4 +15,6 @@ function draw() {
   ball1.display();
   ball2.update();
   ball2.display();
+  ball3.update();
+  ball3.display();
 }
