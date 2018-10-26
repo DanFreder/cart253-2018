fbce050b8f8af8c20950783d5157d4e77a94de97
DanFreder <freder.daniel@gmail.com>
2018-10-25 22:24:02 -0400

    Ex5: Updated Score Text Colour & Size

--- exercises/exercise5/js/Score.js
+++ exercises/exercise5/js/Score.js
@@ -1,22 +1,61 @@
-// How we keep track of the Score
+/////NEW/////
+
+// How we keep track of the Score + draw it onscreen
 
 var leftPadScore = 0;
 var rightPadScore = 0;
 
 function Score(size) {
-this.size = size;
+  this.size = size;
 }
 
-
-// Draw the Score onscreen
 Score.prototype.display = function() {
-push();
-strokeWeight(10);
-fill(251,54,64);
-textSize(this.size);
-textStyle(ITALIC, BOLD);
-textAlign(this.x,this.y);
-text("Right Paddle: " + rightPadScore, width - 140, 450);
-text("Left Paddle: " + leftPadScore, width - 600, 450);
-pop();
+  if (leftPadScore === rightPadScore) {
+    push();
+    fill(76, 159, 112);
+    strokeWeight(10);
+    textSize(this.size);
+    textStyle(ITALIC, BOLD);
+    textAlign(this.x, this.y);
+    text("Left Paddle: " + leftPadScore, width - 600, 450);
+    text("Right Paddle: " + rightPadScore, width - 140, 450);
+    pop();
+  }
+  if (leftPadScore > rightPadScore) {
+    push();
+    fill(251, 54, 64);
+    strokeWeight(10);
+    textSize(this.size);
+    textStyle(ITALIC, BOLD);
+    textAlign(this.x, this.y);
+    text("Right Paddle: " + rightPadScore, width - 140, 450);
+    pop();
+    push();
+    fill(76, 159, 112);
+    strokeWeight(10);
+    textSize(this.size);
+    textStyle(ITALIC, BOLD);
+    textAlign(this.x, this.y);
+    text("Left Paddle: " + leftPadScore, width - 600, 450);
+    pop();
+  }
+  if (rightPadScore > leftPadScore) {
+    push();
+    fill(76, 159, 112);
+    strokeWeight(10);
+    textSize(this.size);
+    textStyle(ITALIC, BOLD);
+    textAlign(this.x, this.y);
+    text("Right Paddle: " + rightPadScore, width - 140, 450);
+    pop();
+    push();
+    fill(251, 54, 64);
+    strokeWeight(10);
+    textSize(this.size);
+    textStyle(ITALIC, BOLD);
+    textAlign(this.x, this.y);
+    text("Left Paddle: " + leftPadScore, width - 600, 450);
+    pop();
+  }
 }
+/////END NEW/////
