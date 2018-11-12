1d0748272f5cf6e739afda94d5cb47fe84f8e2cd
DanFreder <freder.daniel@gmail.com>
2018-11-12 13:50:25 -0500

    Ex7: Started Exercise 7
    Toying around with 3D shapes

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,82 @@
+var o1x = 0;
+var o1y = 0;
+var o1z = 0;
+var o2x = 0;
+var o2y = 0;
+var o2z = 0;
+var o3x = 0;
+var o3y = 0;
+var o3z = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+var o1x = random(1);
+var o1y = random(1);
+var o1z = random(1);
+var o2x = random(1);
+var o2y = random(1);
+var o2z = random(1);
+var o3x = random(1);
+var o3y = random(1);
+var o3z = random(1);
+}
+function draw() {
+  background(0);
+
+  ///stylish rectangles
+  push();
+  translate(0,0,0);
+  strokeWeight(2);
+  stroke(36,123,160);
+  fill(0);
+  rectMode(CENTER);
+  rect(3,3,windowWidth - 300, windowHeight - 200);
+  rect(-3,-3,windowWidth - 300, windowHeight - 200);
+  pop();
+
+///BLUE CUBE
+  push();
+  translate(-10, -5, -3);
+    strokeWeight(1);
+    stroke(0);
+  rotateX(o1x);
+  rotateY(o1y);
+  rotateZ(o1z);
+  fill(10,36,99);
+  box(300, 300, 300,4,4);
+  o1x += .002;
+  o1y += .004;
+  o1z += .003;
+  pop();
+
+///RED CUBE
+  push();
+  translate(10, 5, 3);
+    strokeWeight(1);
+      stroke(0);
+  rotateX(o1x);
+  rotateY(o1y);
+  rotateZ(o1z);
+  fill(251,54,64);
+  box(300, 300, 300,4,4);
+  o2x += .004;
+  o2y += .003;
+  o2z += .002;
+  pop();
+
+///GREY CUBE
+  push();
+  translate(0, 0, 0);
+  strokeWeight(1);
+    stroke(0);
+  rotateX(o3x);
+  rotateY(o3y);
+  rotateZ(o3z);
+  fill(96,95,94);
+  box(300, 300, 300,4,4);
+  o3x += .003;
+  o3y += .002;
+  o3z += .004;
+  pop();
+
+}
--- exercises/inClassFuckery/js/script.js
+++ exercises/inClassFuckery/js/script.js
@@ -1,6 +1,12 @@
-var sunAngle = 0;
-var moonAngle = 0;
-var earthAngle = 0;
+var o1x = 0;
+var o1y = 0;
+var o1z = 0;
+var o2x = 0;
+var o2y = 0;
+var o2z = 0;
+var o3x = 0;
+var o3y = 0;
+var o3z = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -12,23 +18,39 @@ function draw() {
   background(0);
 
   push();
-translate(0,0,-10);
-  rotateZ(earthAngle);
-  fill(226,226,226, 230);
-    box(300, 300, 300);
-  earthAngle += .003;
-pop();
-
-push();
-translate(0,0,10);
-  rotateX(moonAngle);
-  fill(251,54,64, 230);
+  translate(0, 0, -10);
+  rotateX(o1x);
+  rotateY(o1y);
+  rotateZ(o1z);
+  fill(226, 226, 226, 230);
   box(300, 300, 300);
-  moonAngle += .002;
+  o1x += .003;
+  o1y += .004;
+  o1z += .005;
   pop();
 
-  rotateY(sunAngle);
-  fill(10,36,99,230);
+  push();
+  translate(0, 0, 10);
+  rotateX(o1x);
+  rotateY(o1y);
+  rotateZ(o1z);
+  fill(251, 54, 64, 230);
   box(300, 300, 300);
-  sunAngle += .001;
+  o2x += .005;
+  o2y += .004;
+  o2z += .003;
+  pop();
+
+  push();
+  translate(0, 0, 10);
+  rotateX(o3x);
+  rotateY(o3y);
+  rotateZ(o3z);
+  fill(10, 36, 99, 230);
+  box(300, 300, 300);
+  o3x += .004;
+  o3y += .003;
+  o3z += .005;
+  pop();
+
 }
