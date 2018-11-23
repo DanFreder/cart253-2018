9531d714f43e371887ba40feaf4ed46a6f4a5022
DanFreder <freder.daniel@gmail.com>
2018-11-22 19:06:58 -0500

    Project3: Sneaky Tuber 4 set update

--- project3/SneakySneaky/js/script.js
+++ project3/SneakySneaky/js/script.js
@@ -3,19 +3,21 @@
 Sneaky Tubers
 Dan Freder
 
-4 semi-transparent circles leave trails on the canvas
+4 semi-transparent circle-sets leave trails on the canvas
 
 ******************/
 var x;
 var y;
 var vx;
 var vy;
-var maxSpeed = 15;
+var maxSpeed = 8;
 var tx;
 var ty;
 
 var circle;
 var circle2;
+var circle3;
+var circle4;
 
 function preload() {}
 
@@ -30,15 +32,22 @@ function setup() {
   canvas.style("left:0");
   canvas.style("z-index:-100");
 
-circle = new Circle(windowWidth/2-300,windowHeight/2,100);
-circle2 = new Circle(windowWidth/2+300,windowHeight/2,100);
+circle = new Circle(windowWidth/2-160,windowHeight/2,100);
+circle2 = new Circle(windowWidth/2+160,windowHeight/2,100);
+circle3 = new Circle(windowWidth/2,windowHeight/2+90,100);
+circle4 = new Circle(windowWidth/2,windowHeight/2-90,100);
+
 }
 
 function draw() {
   circle.update();
   circle2.update();
+  circle3.update();
+  circle4.update();
   circle.display();
+  circle3.display();
   circle2.display();
+  circle4.display();
 }
 
 function windowResized() {
