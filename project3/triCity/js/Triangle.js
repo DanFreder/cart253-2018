bc19cea407470c678dffaec23c6be64c3aef77c1
DanFreder <freder.daniel@gmail.com>
2018-12-02 22:05:02 -0500

    Project3: Tri-city Updated for Keyboard Controls

--- project3/triCity/js/Triangle.js
+++ project3/triCity/js/Triangle.js
@@ -2,22 +2,42 @@ function Triangle(offsetX,offsetY,direction) {
   this.offsetX = offsetX;
   this.offsetY = offsetY;
   this.direction = direction;
-
   // -1 or 1 for direction
 }
 
 Triangle.prototype.update = function() {
+  //find center of each triangle
   oX = ((aX + bX + cX) / 3);
   oY = ((aY + bY + cY) / 3);
 
-  //find center of each triangle
+  //keyboard arrow keys control vertices
+  if (keyIsDown(LEFT_ARROW)) {
+    aX -= 0.1;
+    bX -= 0.1;
+    cX -= 0.1;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    aX += 0.1;
+    bX += 0.1;
+    cX += 0.1;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    aY -= 0.1;
+    bY -= 0.1;
+    cY -= 0.1;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    aY += 0.1;
+    bY += 0.1;
+    cY += 0.1;
+  }
 }
 
 Triangle.prototype.display = function() {
 
   push();
-  fill(255, 255, 255, 2);
-  stroke(0,0,0,150);
+  fill(245, 237, 240, 3);
+  stroke(66,76,85,255);
   strokeWeight(2);
   translate(oX,oY);
   rotate(radians(frameCount) * this.direction);
@@ -25,5 +45,4 @@ Triangle.prototype.display = function() {
           this.offsetX + bX, this.offsetY + bY,
           this.offsetX + cX, this.offsetY+ cY);
   pop();
-
   }
--- project3/triCity/js/script.js
+++ project3/triCity/js/script.js
@@ -1,26 +1,25 @@
 /*****************
 
-Nu triangle Shit???
+Tri-city
 Dan Freder
+
+An array of triangles rotate + leave trails on the canvas
 
 ******************/
 
-var numTriangles = 10;
+var numTriangles = 75;
 var triangles = [];
 
-var aX = -500;
-var aY = -280.8676666667;
-var bX = 10;
-var bY = 570.7353333333;
-var cX = 500;
-var cY = -280.8676666667;
-
-function preload() {}
+var aX = -400;
+var aY = -344;
+var bX = 0;
+var bY = 348.84;
+var cX = 400;
+var cY = -344;
 
 function setup() {
   // Create a canvas the size of the window
   canvas = createCanvas(windowWidth, windowHeight);
-  background(30);
   // Style it so it sits fixed behind HTML & ignores scrolling
   canvas.style("display:block");
   canvas.style("position:fixed");
@@ -28,31 +27,28 @@ function setup() {
   canvas.style("left:0");
   canvas.style("z-index:-100");
 
-
 for (var i = 0; i < numTriangles; i++) {
 
-  if (i % 3 === 1) {
-  triangles[i] = new Triangle(0,0,(abs(random(-1,1))));
+  if (i % 2 === 1) {
+  triangles[i] = new Triangle(100*i,-10*i,random(-1,1)*1.5);
 }
-  else if (i % 2 === 1) {
-    triangles[i] = new Triangle(-300,0,(abs(random(-1,1))));
-  }
   else {
-    triangles[i] = new Triangle(300,0,(abs(random(-1,1))));
+    triangles[i] = new Triangle(-100*i,10*i,random(-1,1)*1.5);
+  }
 }
-}
+background(0);
 }
 
 function draw() {
 translate(windowWidth/2,windowHeight/2);
-  for (var i = 0; i < numTriangles; i++) {
+for (var i = 0; i < numTriangles; i++) {
 triangles[i].update();
-  triangles[i].display();
+triangles[i].display();
 }
 }
 
 function windowResized() {
   // resize our canvas to the new window dimensions
   resizeCanvas(windowWidth, windowHeight);
-  background(50);
+  background(0);
 }
